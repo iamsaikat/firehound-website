4d19ed1c027ca3f259537d0c59e8828631dbeec7
Pranay Narang <pranay@firehound.org>
2018-06-25 01:10:19 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api: {
-    rooturl: 'https://api.firehound.org/nodejs/api/devices'
+    rooturl: 'https://api.firehound.org/nodejs/api'
   }
 };
